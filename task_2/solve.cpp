b317e4370177d90b15f1ae193ff241da763880bd
NickolayYakovchukfm <creepertrase@gmail.com>
2022-07-08 23:21:45 +0300

    modified code

--- task_2/solve.cpp
+++ task_2/solve.cpp
@@ -2,23 +2,40 @@
 #include <cmath>
 #include <iomanip>
 
+/* находит длину вектора на точках
+    х1  координаты по х первой точки
+    y1  координаты по y первой точки
+
+    x2  координаты по х второй точки
+    y2  координаты по y второй точки
+
+    return длина вектора на заданных точках
+*/
+double getVectorLenght(int x1, int y1,
+                       int x2, int y2);
+
 int main() {
-    int X_1 = 0,
-        Y_1 = 0,
-        X_2 = 0,
-        Y_2 = 0,
-        X = 0,
-        Y = 0;
+    int x1 = 0,
+        y1 = 0,
+        x2 = 0,
+        y2 = 0;
 
-    std::cin >> X_1 >> Y_1 >> X_2 >> Y_2;
+    std::cin >> x1 >> y1 >> x2 >> y2;
 
-    X = X_2 - X_1;
-    Y = Y_2 - Y_1;
+    double ans = getVectorLenght(x1, y1, x2, y2);
 
-    double ans = 0;
-    ans = std::sqrt(std::pow(X, 2) + std::pow(Y, 2));
+    std::cout << std::setprecision(5) << std::fixed << ans << std::endl;
 
-    std::cout << std::setprecision(5) << std::fixed;
-    std::cout << ans << std::endl;
     return 0;
+}
+
+double getVectorLenght(int x1, int y1,
+                       int x2, int y2)
+{
+    int x0 = x2 - x1,
+        y0 = y2 - y1;
+
+    double lenght = std::sqrt(std::pow(x0, 2) + std::pow(y0, 2));
+
+    return lenght;
 }
