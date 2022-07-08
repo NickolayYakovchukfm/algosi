9a627e556496223fc1ca1f7216604add31f43fa3
NickolayYakovchukfm <creepertrase@gmail.com>
2022-07-08 21:46:03 +0300

    reworked

--- task_1/solve.cpp
+++ task_1/solve.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int main() {
-	int A = 0,
-		B = 0;
+    int A = 0,
+        B = 0;
 
-	std::cin >> A >> B;
+    std::cin >> A >> B;
 
-	int ans = 0;
-	ans = A + B;
+    int ans = 0;
+    ans = A + B;
 
-	std::cout << ans << std::endl;
-	return 0;
+    std::cout << ans << std::endl;
+    return 0;
 }
