89239396762f139f427f50536530fb6ba146782c
NickolayYakovchukfm <creepertrase@gmail.com>
2022-07-12 11:59:29 +0300

    last fix

--- task_131/solve.cpp
+++ task_131/solve.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 
 int main() {
-    int N = 0,
-        V = 0,
-        S = 0,
-   number = 0,
-    max_V = 0;
+    int n = 0,
+        v = 0,
+        s = 0;
+
+    std::cin >> n;
+
+    int number = 0,
+         max_v = 0;
 
     bool flag = 0;
 
-    std::cin >> N;
+    for (int i = 1; i <= n; i++){
+        std::cin >> v >> s;
 
-    for (int i = 1; i <= N; i++) {
-        std::cin >> V >> S;
-
-        if ((1 == S) && (V > max_V))
-        {
-            flag = 1;
-            max_V = V;
-            number = i;
-        } else 
-        
+        if (0 == s)
         {
             continue;
         }
 
+        if ((1 == s) && (v > max_v))
+        {
+            flag = 1;
+            max_v = v;
+            number = i;
+        }
     }
 
-    if (flag == 0)
+    if (!flag)
     {
         std::cout << -1 << std::endl;
 
